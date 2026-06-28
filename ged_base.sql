275a9906f99650a5a4b957a79a68686d3ba6070c
Alessandro Aparecido Da Silva Domingues <alessandrosilvaicas.enf@gmail.com>
2026-06-28 17:02:17 -0300

    Add files via upload

---
+++ ged_base.sql
@@ -0,0 +1,387 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 28/06/2026 às 22:01
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `ged`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `app_settings`
+--
+-- Erro ao ler a estrutura para a tabela ged.app_settings: #1932 - Table &#039;ged.app_settings&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.app_settings: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`app_settings`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `arquivo`
+--
+-- Erro ao ler a estrutura para a tabela ged.arquivo: #1932 - Table &#039;ged.arquivo&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.arquivo: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`arquivo`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `assinaturas`
+--
+-- Erro ao ler a estrutura para a tabela ged.assinaturas: #1932 - Table &#039;ged.assinaturas&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.assinaturas: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`assinaturas`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `auditoria`
+--
+-- Erro ao ler a estrutura para a tabela ged.auditoria: #1932 - Table &#039;ged.auditoria&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.auditoria: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`auditoria`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `configuracoes`
+--
+-- Erro ao ler a estrutura para a tabela ged.configuracoes: #1932 - Table &#039;ged.configuracoes&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.configuracoes: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`configuracoes`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `documentos`
+--
+-- Erro ao ler a estrutura para a tabela ged.documentos: #1932 - Table &#039;ged.documentos&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.documentos: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`documentos`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `documentos_antigo`
+--
+-- Erro ao ler a estrutura para a tabela ged.documentos_antigo: #1932 - Table &#039;ged.documentos_antigo&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.documentos_antigo: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`documentos_antigo`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `documentos_assinaturas`
+--
+-- Erro ao ler a estrutura para a tabela ged.documentos_assinaturas: #1932 - Table &#039;ged.documentos_assinaturas&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.documentos_assinaturas: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`documentos_assinaturas`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `documentos_assinaturas_convites`
+--
+-- Erro ao ler a estrutura para a tabela ged.documentos_assinaturas_convites: #1932 - Table &#039;ged.documentos_assinaturas_convites&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.documentos_assinaturas_convites: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`documentos_assinaturas_convites`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `documentos_indice`
+--
+-- Erro ao ler a estrutura para a tabela ged.documentos_indice: #1932 - Table &#039;ged.documentos_indice&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.documentos_indice: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`documentos_indice`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `documentos_ocr`
+--
+-- Erro ao ler a estrutura para a tabela ged.documentos_ocr: #1932 - Table &#039;ged.documentos_ocr&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.documentos_ocr: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`documentos_ocr`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `documento_funcoes`
+--
+-- Erro ao ler a estrutura para a tabela ged.documento_funcoes: #1932 - Table &#039;ged.documento_funcoes&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.documento_funcoes: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`documento_funcoes`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `documento_metadados`
+--
+-- Erro ao ler a estrutura para a tabela ged.documento_metadados: #1932 - Table &#039;ged.documento_metadados&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.documento_metadados: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`documento_metadados`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `documento_versoes_antigo`
+--
+-- Erro ao ler a estrutura para a tabela ged.documento_versoes_antigo: #1932 - Table &#039;ged.documento_versoes_antigo&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.documento_versoes_antigo: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`documento_versoes_antigo`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `doc_templates`
+--
+-- Erro ao ler a estrutura para a tabela ged.doc_templates: #1932 - Table &#039;ged.doc_templates&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.doc_templates: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`doc_templates`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `emails_log`
+--
+-- Erro ao ler a estrutura para a tabela ged.emails_log: #1932 - Table &#039;ged.emails_log&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.emails_log: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`emails_log`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `email_templates`
+--
+-- Erro ao ler a estrutura para a tabela ged.email_templates: #1932 - Table &#039;ged.email_templates&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.email_templates: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`email_templates`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `entidade`
+--
+-- Erro ao ler a estrutura para a tabela ged.entidade: #1932 - Table &#039;ged.entidade&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.entidade: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`entidade`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `exercicio`
+--
+-- Erro ao ler a estrutura para a tabela ged.exercicio: #1932 - Table &#039;ged.exercicio&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.exercicio: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`exercicio`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `funcao_permissao`
+--
+-- Erro ao ler a estrutura para a tabela ged.funcao_permissao: #1932 - Table &#039;ged.funcao_permissao&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.funcao_permissao: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`funcao_permissao`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `funcoes`
+--
+-- Erro ao ler a estrutura para a tabela ged.funcoes: #1932 - Table &#039;ged.funcoes&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.funcoes: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`funcoes`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `ingest_arquivos`
+--
+-- Erro ao ler a estrutura para a tabela ged.ingest_arquivos: #1932 - Table &#039;ged.ingest_arquivos&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.ingest_arquivos: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`ingest_arquivos`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `ingest_eventos`
+--
+-- Erro ao ler a estrutura para a tabela ged.ingest_eventos: #1932 - Table &#039;ged.ingest_eventos&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.ingest_eventos: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`ingest_eventos`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `lbr`
+--
+-- Erro ao ler a estrutura para a tabela ged.lbr: #1932 - Table &#039;ged.lbr&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.lbr: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`lbr`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `logs`
+--
+-- Erro ao ler a estrutura para a tabela ged.logs: #1932 - Table &#039;ged.logs&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.logs: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`logs`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `mes`
+--
+-- Erro ao ler a estrutura para a tabela ged.mes: #1932 - Table &#039;ged.mes&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.mes: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`mes`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `metadado_campos`
+--
+-- Erro ao ler a estrutura para a tabela ged.metadado_campos: #1932 - Table &#039;ged.metadado_campos&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.metadado_campos: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`metadado_campos`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pastas`
+--
+-- Erro ao ler a estrutura para a tabela ged.pastas: #1932 - Table &#039;ged.pastas&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.pastas: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`pastas`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `permissoes`
+--
+-- Erro ao ler a estrutura para a tabela ged.permissoes: #1932 - Table &#039;ged.permissoes&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.permissoes: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`permissoes`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `relacaosessao`
+--
+-- Erro ao ler a estrutura para a tabela ged.relacaosessao: #1932 - Table &#039;ged.relacaosessao&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.relacaosessao: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`relacaosessao`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `sessao`
+--
+-- Erro ao ler a estrutura para a tabela ged.sessao: #1932 - Table &#039;ged.sessao&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.sessao: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`sessao`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tipoarquivo`
+--
+-- Erro ao ler a estrutura para a tabela ged.tipoarquivo: #1932 - Table &#039;ged.tipoarquivo&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.tipoarquivo: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`tipoarquivo`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tiposessao`
+--
+-- Erro ao ler a estrutura para a tabela ged.tiposessao: #1932 - Table &#039;ged.tiposessao&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.tiposessao: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`tiposessao`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tipos_documento`
+--
+-- Erro ao ler a estrutura para a tabela ged.tipos_documento: #1932 - Table &#039;ged.tipos_documento&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.tipos_documento: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`tipos_documento`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tipo_documento_funcoes`
+--
+-- Erro ao ler a estrutura para a tabela ged.tipo_documento_funcoes: #1932 - Table &#039;ged.tipo_documento_funcoes&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.tipo_documento_funcoes: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`tipo_documento_funcoes`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario`
+--
+-- Erro ao ler a estrutura para a tabela ged.usuario: #1932 - Table &#039;ged.usuario&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.usuario: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`usuario`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+-- Erro ao ler a estrutura para a tabela ged.usuarios: #1932 - Table &#039;ged.usuarios&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.usuarios: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`usuarios`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuariosessao`
+--
+-- Erro ao ler a estrutura para a tabela ged.usuariosessao: #1932 - Table &#039;ged.usuariosessao&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.usuariosessao: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`usuariosessao`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario_certificados`
+--
+-- Erro ao ler a estrutura para a tabela ged.usuario_certificados: #1932 - Table &#039;ged.usuario_certificados&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.usuario_certificados: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`usuario_certificados`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `workflows`
+--
+-- Erro ao ler a estrutura para a tabela ged.workflows: #1932 - Table &#039;ged.workflows&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.workflows: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`workflows`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `workflow_aprovacoes`
+--
+-- Erro ao ler a estrutura para a tabela ged.workflow_aprovacoes: #1932 - Table &#039;ged.workflow_aprovacoes&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.workflow_aprovacoes: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`workflow_aprovacoes`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `workflow_aprovadores`
+--
+-- Erro ao ler a estrutura para a tabela ged.workflow_aprovadores: #1932 - Table &#039;ged.workflow_aprovadores&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.workflow_aprovadores: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`workflow_aprovadores`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `workflow_documentos`
+--
+-- Erro ao ler a estrutura para a tabela ged.workflow_documentos: #1932 - Table &#039;ged.workflow_documentos&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.workflow_documentos: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`workflow_documentos`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `workflow_etapas`
+--
+-- Erro ao ler a estrutura para a tabela ged.workflow_etapas: #1932 - Table &#039;ged.workflow_etapas&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.workflow_etapas: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`workflow_etapas`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `workflow_notificacoes`
+--
+-- Erro ao ler a estrutura para a tabela ged.workflow_notificacoes: #1932 - Table &#039;ged.workflow_notificacoes&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela ged.workflow_notificacoes: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `ged`.`workflow_notificacoes`&#039; na linha 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
